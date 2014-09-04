29430838e0935d3b8aa7fc855ab9f2b8cdd19527
Erispoe <favrebulle@gmail.com>
2014-09-04 10:20:32 +0200

    Closeness

--- closeness.js
+++ closeness.js
@@ -1,30 +1,78 @@
 $(document).ready(function() {
-  var zip = {zip};
-  var url = "http://swissmadearchitects.com/zipmetro-api/index.php?key=snickers&zip=" + zip;
-  $.get(url).done(function(data){
-    d = JSON.parse(data);
-    console.log(d);
-    $("#county").html(d.countynamefull);
-    $("#state").html("the state of " + d.StateName);
-    
-    // Insert metro area data, remove question if the respondent is not in a metro area
-    if(d.PSATitle != null) {
-   		$("#cbsa").html(d.PSATitle);
-    	$("#cbsacentral").html("the city of " + d.PSACentralCity);
-    } else {
-    	$("#javatbd367231X21X1031SQ005").remove();
-    	$("#javatbd367231X21X1031SQ006").remove();
-    }
+  var zipR = {Loc_ResZip} + "";
+  var zipW = {Loc_WorkStudyZip} + "";
 
-    // Insert place name, as answered in a previous question
-    place = '{INSERTANS:367231X21X1042}';
-    if(place != '-oth-') {
-    	i = parseInt(place);
-    	$("#city").html('the town/city of ' + d.cities[i-1]);
+  var replace = function(dR, dW) {
+    // Residential variables
+    console.log(dR.StateName);
+    $("#countyR").html(dR.countynamefull); // County of residence
+    $("#stateR").html("The State of " + dR.StateName); // State of residence
+
+    // Insert residential place name, as answered in a previous question
+    placeR = '{INSERTANS:995335X29X1411}';
+    if(placeR != '-oth-') {
+        i = parseInt(placeR);
+        $("#placeR").html(dR.cities[i-1]);
     } else {
-    	poth = '{INSERTANS:367231X21X1042other}';
-    	$("#city").html('the town/city of ' + poth);
+        pRoth = '{INSERTANS:995335X29X1411other}';
+        $("#placeR").html(pRoth);
     };
 
-  });
+    // Insert metro area data, remove question if the respondent is not in a metro area
+    if(dR.PSATitle != null) {
+        $("#metroR").html(", " + dR.PSATitle);
+        $("#centralR").html(dR.PSACentralCity);
+    } else {
+        $("#javatbd995335X26X1106SQ004").remove();
+        $("#javatbd995335X26X1106SQ008").remove();
+    }
+
+    if(dW == 0) {
+        $("#javatbd995335X26X1106SQ010").remove(); // Remove place of work
+        $("#javatbd995335X26X1106SQ009").remove(); // Remove county of work
+    } else {
+        $("#countyW").html(dW.countynamefull); // Insert county of work
+
+        // Insert place of work
+
+        placeW = '{INSERTANS:995335X29X1413}';
+        if(placeW != '-oth-') {
+            i = parseInt(placeW);
+            $("#placeW").html(dW.cities[i-1]);
+        } else {
+            pWoth = '{INSERTANS:995335X29X1413other}';
+            $("#placeW").html(pWoth);
+        };
+    };
+
+
+  }
+
+
+  // ZipAPI url to get infos for residential and work places
+
+  if(zipR != 0) {
+    var urlR = "http://swissmadearchitects.com/zipmetro-api/index.php?key=snickers&zip=" + zipR;  
+    $.get(urlR).done(function(dataR){
+        var dR = JSON.parse(dataR);
+        var dW = 0;
+        console.log(dR);
+
+
+        if(zipW != 0) {
+            var urlW = "http://swissmadearchitects.com/zipmetro-api/index.php?key=snickers&zip=" + zipW;
+          
+            $.get(urlW).done(function(dataW){
+                dW = JSON.parse(dataW);
+                console.log(dW);
+                replace(dR, dW);
+            });
+        } else {
+            replace(dR, dW);
+        }
+
+    
+
+    });
+  }
 });
