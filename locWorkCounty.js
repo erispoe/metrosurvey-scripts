b238aac3887a8c62de37df3655f72dbd9591c0da
Erispoe <favrebulle@gmail.com>
2014-10-01 15:15:31 -0700

    Answer automatically if county is not found

--- locWorkCounty.js
+++ locWorkCounty.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  var zip = {Loc_WorkStudyZip};
-  var url = "http://swissmadearchitects.com/zipmetro-api/index.php?key=snickers&zip=" + zip;
+  var url = "http://swissmadearchitects.com/zipmetro-api/index.php?key=snickers&zip=" + zipW;
   $.get(url).done(function(data){
+  	if(data == ""){
+    	// Automatically answer No if zip is not found
+    	$("[id^=tick_img_answer]")[1].click();
+    	$('#limesurvey').submit();
+    }
     d = JSON.parse(data);
     console.log(d);
     var txt = "Is <strong>" + d.countynamefull + ", " + d.StateName + "</strong> the county of your main place of work or study?";
